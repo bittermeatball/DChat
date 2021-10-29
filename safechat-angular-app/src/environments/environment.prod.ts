8b4835280b089f2b32ced0d3544c514b411ec2a1
dangphu2412 <dangphu241299@gmail.com>
2021-10-29 22:35:25 +0700

    Set domain connect in backend

--- safechat-angular-app/src/environments/environment.prod.ts
+++ safechat-angular-app/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://34.68.231.254/v1/chain',
+  apiUrl: 'https://dchat-be.sgroupvn.org/v1/chain',
   peerHost: '35.226.197.45',
   peerPort: '9000',
 };
--- safechat-angular-app/src/environments/environment.ts
+++ safechat-angular-app/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://34.68.231.254/v1/chain',
+  apiUrl: 'https://dchat-be.sgroupvn.org/v1/chain',
   peerHost: '35.226.197.45',
   peerPort: '9000',
 };
